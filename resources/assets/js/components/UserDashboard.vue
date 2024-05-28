66387b4a4f6ed385f29cfbf9997d5f49d6b7d00d
Alen T Santhosh <139475989+alentsanthosh@users.noreply.github.com>
2024-05-28 17:41:05 +0530

    Update UserDashboard.vue

--- resources/assets/js/components/UserDashboard.vue
+++ resources/assets/js/components/UserDashboard.vue
@@ -1,17 +1,45 @@
 <template>
-<div>
+  <div>
     <h5 class="heading">User Dashboard page</h5>
-</div>
+    <button @click="logout" class="logout-button">Logout</button>
+  </div>
 </template>
+
 <script>
+import axios from 'axios';
+
 export default {
-    
+  methods: {
+    logout() {
+      axios.post('/logout')
+        .then(response => {
+          console.log(response.data.message);
+          this.$router.push('/login');
+        })
+        .catch(error => {
+          console.error(error.response.data.error);
+        });
+    }
+  }
 }
 </script>
+
 <style scoped>
-.heading{
-    position:fixed;
-    left: 43%;
-    margin-top: 100px;
+.heading {
+  position: fixed;
+  left: 43%;
+  margin-top: 100px;
+}
+
+.logout-button {
+  margin-top: 200px;
+  position: fixed;
+  left: 47%;
+  border: none;
+  border-radius: 6px;
+  background-color: #ff0000;
+  color: white;
+  padding: 10px 20px;
+  cursor: pointer;
 }
 </style>
